792bf72780c047d144385d0772e4736184649e09
ruvosipenk <Vladislav.Osipenkov@harman.com>
2017-02-06 10:11:31 +0300

    check style fix

--- src/clients/ClientNameGenerator.java
+++ src/clients/ClientNameGenerator.java
@@ -1,12 +1,14 @@
 package clients;
 
 public class ClientNameGenerator {
-	private static String[] names = { "intel", "AMD", "Siemens", "Motorolla", "Verison", "Beeline", "Harman", "Yota",
-			"Liberty", "Aeroflot", "Gazprom", "Taxe Veset", "Nelsen", "Clever", "Anritsu", "Mera", "Appolo", "BBC" };
+    private static String[] names = {"intel", "AMD", "Siemens", "Motorolla", "Verison", "Beeline", "Harman",
+            "Yota",
+            "Liberty", "Aeroflot", "Gazprom", "Taxe Veset", "Nelsen", "Clever", "Anritsu", "Mera", "Appolo",
+            "BBC" };
 
-	public static String getName() {
-		int index = (int) (Math.random() * names.length);
+    public static String getName() {
+        int index = (int) (Math.random() * names.length);
 
-		return names[index];
-	}
+        return names[index];
+    }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -13,96 +13,95 @@ import java.nio.file.Paths;
  * Server Server version 2.
  * 
  * @author 21cmPC
- *
  */
 public class Server {
 
-	private static int clientIndex = 0;
-	private ServerSocket serverSocket;
-	private static Server instance;
-	private final int SERVER_PORT = 7777;
-	private final String PHRASES_FILE_PATH = "src/server/phrases.txt";
-	private final String LOG_FILE_PATH = "src/server/log.txt";
+    private static int clientIndex = 0;
+    private ServerSocket serverSocket;
+    private static Server instance;
+    private final int SERVER_PORT = 7777;
+    private final String PHRASES_FILE_PATH = "src/server/phrases.txt";
+    private final String LOG_FILE_PATH = "src/server/log.txt";
 
-	/**
-	 * Creates server instance.
-	 * 
-	 * @throws IOException
-	 */
-	private Server() throws IOException {
-		serverSocket = new ServerSocket(SERVER_PORT);
-		System.err.println("Server started");
-	}
+    /**
+     * Creates server instance.
+     * 
+     * @throws IOException
+     */
+    private Server() throws IOException {
+        serverSocket = new ServerSocket(SERVER_PORT);
+        System.err.println("Server started");
+    }
 
-	/*
-	 * Returns instance of Server.
-	 */
-	public static Server getInstance() throws IOException {
-		if (instance == null) {
-			instance = new Server();
-		}
+    /*
+     * Returns instance of Server.
+     */
+    public static Server getInstance() throws IOException {
+        if (instance == null) {
+            instance = new Server();
+        }
 
-		return instance;
-	}
+        return instance;
+    }
 
-	/*
-	 * Start command for server.
-	 */
-	public void serverStart() throws IOException {
-		while (true) {
-			Socket socket = serverSocket.accept();
-			Thread newClientThread = new Thread(new Runnable() {
+    /*
+     * Start command for server.
+     */
+    public void serverStart() throws IOException {
+        while (true) {
+            Socket socket = serverSocket.accept();
+            Thread newClientThread = new Thread(new Runnable() {
 
-				@Override
-				public void run() {
-					Socket clientSocket = socket;
-					PrintWriter writer;
-					BufferedReader reader;
-					try {
-						writer = new PrintWriter(socket.getOutputStream());
-						reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                @Override
+                public void run() {
+                    Socket clientSocket = socket;
+                    PrintWriter writer;
+                    BufferedReader reader;
+                    try {
+                        writer = new PrintWriter(socket.getOutputStream());
+                        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-						writer.println(generatePhrase());
-						writer.flush();
-						String clientName = reader.readLine();
-						System.out.println(clientName);
-						log(clientName);
+                        writer.println(generatePhrase());
+                        writer.flush();
+                        String clientName = reader.readLine();
+                        System.out.println(clientName);
+                        log(clientName);
 
-						clientSocket.close();
-						if (clientSocket.isClosed()) {
-							System.out.println("Connection with " + clientName + " closed successfully");
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			});
-			newClientThread.start();
-		}
-	}
+                        clientSocket.close();
+                        if (clientSocket.isClosed()) {
+                            System.out.println("Connection with " + clientName + " closed successfully");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            newClientThread.start();
+        }
+    }
 
-	private void log(String clientName) throws IOException {
-		Files.write(Paths.get(LOG_FILE_PATH), clientName.getBytes());
-	}
+    private void log(String clientName) throws IOException {
+        Files.write(Paths.get(LOG_FILE_PATH), clientName.getBytes());
+    }
 
-	private String generatePhrase() throws IOException {
-		String fileContent = new String(Files.readAllBytes(Paths.get(PHRASES_FILE_PATH)));
-		String[] lines = fileContent.split("\n+");
-		int index = (int) (Math.random() * lines.length);
+    private String generatePhrase() throws IOException {
+        String fileContent = new String(Files.readAllBytes(Paths.get(PHRASES_FILE_PATH)));
+        String[] lines = fileContent.split("\n+");
+        int index = (int) (Math.random() * lines.length);
 
-		return lines[index];
-	}
+        return lines[index];
+    }
 
-	/**
-	 * Deprecated
-	 * 
-	 * @return
-	 */
-	private String generateNameForClient() {
-		return "Client" + clientIndex++;
-	}
+    /**
+     * Deprecated
+     * 
+     * @return
+     */
+    private String generateNameForClient() {
+        return "Client" + clientIndex++;
+    }
 
-	public static void main(String[] args) throws IOException {
-		Server.getInstance().serverStart();
-	}
+    public static void main(String[] args) throws IOException {
+        Server.getInstance().serverStart();
+    }
 }
